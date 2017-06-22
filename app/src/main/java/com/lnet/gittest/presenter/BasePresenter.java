2ae7e398ab6ed9c5339b989f7b3e1b92f6b923d5
pj1993 <1314pj520>
2017-06-22 15:01:46 +0800

    基础分包

---
+++ app/src/main/java/com/lnet/gittest/presenter/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.lnet.gittest.presenter;
+
+/**
+ * Created by pj on 2017/6/22.
+ * Have a good day
+ */
+
+public class BasePresenter {
+}
---
+++ app/src/main/java/com/lnet/gittest/ui/MyActivity.java
@@ -0,0 +1,9 @@
+package com.lnet.gittest.ui;
+
+/**
+ * Created by pj on 2017/6/22.
+ * Have a good day
+ */
+
+public class MyActivity {
+}
